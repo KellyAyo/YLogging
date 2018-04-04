6474d3b6812939011d9c9a8af1b9678cc42d9913
Phillip Kelly-Ayo <Kelly-Ayo@MacBook-Pro.local>
2018-04-04 15:01:53 +0100

    Initial commit

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by Phillip Kelly-Ayo on 04/04/2018.
+//  Copyright © 2018 Phillip Kelly-Ayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
